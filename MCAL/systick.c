cf48cf54c9352ba1038f3d5a44cbaf7faf67a9f1
Saeed Nabawy <saeednabawy28@gmail.com>
2025-04-19 18:02:27 +0200

    Systick.c Edit

--- MCAL/systick.c
+++ MCAL/systick.c
@@ -2,17 +2,17 @@
 #include "systick.h"
 void SysTick_Init(void){
 NVIC_ST_CTRL_R = 0;				
-NVIC_ST_RELOAD_R = 16-1;		
+NVIC_ST_RELOAD_R = 0x00FFFFFF;		
 NVIC_ST_CURRENT_R = 0;			
-NVIC_ST_CTRL_R = 5;					
+NVIC_ST_CTRL_R = 0x00000005;					
 }
 
 void delay_us(int count){
-int i;
-for( i = 0; i<count; i++){
+NVIC_ST_RELOAD_R = count*80;
+NVIC_ST_CURRENT_R = 0;
 while((NVIC_ST_CTRL_R&0x10000)==0){}		
 }
-}
+
 
 void delay_ms(int count){
 int i ;
